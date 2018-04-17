378f19b2b75f02c22d55e80818b5d825a7686232
eavictor <eavictor@me.com>
2018-04-17 13:38:55 +0800

    Add Debian install script, update Ubuntu install script

--- debian-install-docker-ce.bash
+++ debian-install-docker-ce.bash
@@ -6,13 +6,13 @@ apt-get upgrade -y
 apt-get dist-upgrade -y
 
 # install docker requirements
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
 
 # add docker repository GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 
 # add docker repository into apt-get source list
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 
 # install docker community edition
 apt-get update
